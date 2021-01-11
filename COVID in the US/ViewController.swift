de9c3d76c84ef42276c94edecbabe44cc18c271e
Erik Villavera <villaveraerik@gmail.com>
2021-01-11 12:27:22 -0500

    Initial Commit

---
+++ COVID in the US/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID in the US
+//
+//  Created by Erik Villavera on 1/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
